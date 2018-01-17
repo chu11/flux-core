ff3ca1ba3ae707e217d94b0afe4075c568f64fb1
Albert Chu <chu11@llnl.gov>
2018-01-17 15:32:15 -0800

    modules/kvs: Begin kvsroot internal API
    Move destroy_root(), remove_root(), create_root(), lookup_root(),
    and lookup_root_safe() over to new files.
    
    Adjust code appropriately given move.

---
+++ src/modules/kvs/kvsroot.h
@@ -0,0 +1,42 @@
+#ifndef _FLUX_KVS_KVSROOT_H
+#define _FLUX_KVS_KVSROOT_H
+
+#include <stdbool.h>
+#include <flux/core.h>
+#include <czmq.h>
+
+#include "cache.h"
+#include "commit.h"
+#include "waitqueue.h"
+#include "src/common/libutil/blobref.h"
+
+struct kvsroot {
+    char *namespace;
+    int seq;
+    blobref_t ref;
+    commit_mgr_t *cm;
+    waitqueue_t *watchlist;
+    int watchlist_lastrun_epoch;
+    int flags;
+    bool remove;
+};
+
+void remove_root (zhash_t *roothash, const char *namespace);
+
+struct kvsroot *lookup_root (zhash_t *roothash, const char *namespace);
+
+struct kvsroot *lookup_root_safe (zhash_t *roothash, const char *namespace);
+
+struct kvsroot *create_root (zhash_t *roothash,
+                             struct cache *cache,
+                             const char *hash_name,
+                             const char *namespace,
+                             int flags,
+                             flux_t *h,
+                             void *arg);
+
+#endif /* !_FLUX_KVS_KVSROOT_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
