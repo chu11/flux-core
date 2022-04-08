1f57e9913c19d60491bd3b56764b17fb0ea6b407
Albert Chu <chu11@llnl.gov>
2022-04-08 07:52:27 -0700

    job-db: store to db when job goes inactive

--- src/modules/job-db/job_state.c
+++ src/modules/job-db/job_state.c
@@ -28,6 +28,7 @@
 #include "src/common/libidset/idset.h"
 
 #include "job_state.h"
+#include "job-archive.h"
 #include "idsync.h"
 #include "job_util.h"
 
@@ -854,12 +855,28 @@ static void process_next_state (struct list_ctx *ctx, struct job *job)
             /* FLUX_JOB_STATE_SCHED */
             /* FLUX_JOB_STATE_CLEANUP */
             /* FLUX_JOB_STATE_INACTIVE */
+            bool inactive = false;
 
-            if (st->state == FLUX_JOB_STATE_INACTIVE)
+            if (st->state == FLUX_JOB_STATE_INACTIVE) {
                 eventlog_inactive_complete (ctx, job);
+                inactive = true;
+            }
 
             update_job_state_and_list (ctx, job, st->state, st->timestamp);
             zlist_remove (job->next_states, st);
+
+            if (inactive) {
+                assert (job->state == FLUX_JOB_STATE_INACTIVE);
+
+                /* if no eventlog, assume from restart */
+                if (job->eventlog) {
+                    if (job_archive_store (jsctx->actx, job) < 0)
+                        flux_log_error (jsctx->h, "%s: job_archive_store",
+                                        __FUNCTION__);
+                }
+
+                break;
+            }
         }
     }
 }
