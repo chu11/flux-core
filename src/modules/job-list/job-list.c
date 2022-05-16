34e77406f02c2885a3d71aa1aa803930633cac85
Albert Chu <chu11@llnl.gov>
2022-05-16 13:17:51 -0700

    job-list: add job db
    Problem: We would like to store inactive jobs on disk for retrieval
    at a later time.
    
    Solution: Add an sqlite job db to the job-list module.  If the
    job-list module is configured with a database path or if the flux
    broker is configured with a statedir, store all inactive job data
    to the db.

--- src/modules/job-list/job-list.c
+++ src/modules/job-list/job-list.c
@@ -160,6 +160,8 @@ static void list_ctx_destroy (struct list_ctx *ctx)
         flux_msg_handler_delvec (ctx->handlers);
         if (ctx->jsctx)
             job_state_destroy (ctx->jsctx);
+        if (ctx->dbctx)
+            job_db_ctx_destroy (ctx->dbctx);
         if (ctx->idsync_lookups)
             idsync_cleanup (ctx);
         free (ctx);
@@ -167,7 +169,7 @@ static void list_ctx_destroy (struct list_ctx *ctx)
     }
 }
 
-static struct list_ctx *list_ctx_create (flux_t *h)
+static struct list_ctx *list_ctx_create (flux_t *h, int argc, char **argv)
 {
     struct list_ctx *ctx = calloc (1, sizeof (*ctx));
     if (!ctx)
@@ -179,6 +181,10 @@ static struct list_ctx *list_ctx_create (flux_t *h)
         goto error;
     if (!(ctx->jsctx = job_state_create (ctx)))
         goto error;
+    if (!(ctx->dbctx = job_db_setup (h, argc, argv))) {
+        if (errno != ENOTBLK)
+            goto error;
+    }
     if (idsync_setup (ctx) < 0)
         goto error;
     return ctx;
@@ -192,7 +198,7 @@ int mod_main (flux_t *h, int argc, char **argv)
     struct list_ctx *ctx;
     int rc = -1;
 
-    if (!(ctx = list_ctx_create (h))) {
+    if (!(ctx = list_ctx_create (h, argc, argv))) {
         flux_log_error (h, "initialization error");
         goto done;
     }
