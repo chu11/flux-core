696980479f5f8977e4168fe5d9e27b895400ba23
Albert Chu <chu11@llnl.gov>
2022-09-07 20:08:15 -0700

    job-list: handle per-resource ntasks special case
    Problem: Under several special cases to work around Jobspec V1
    limitations, the shell per-resource options are used to specify
    number of tasks in a job.  This case was not handled by job-list.
    
    Solution: Handle this special case and set ntasks accordingly.

--- src/modules/job-list/job_state.c
+++ src/modules/job-list/job_state.c
@@ -26,6 +26,7 @@
 #include "src/common/libjob/job_hash.h"
 #include "src/common/librlist/rlist.h"
 #include "src/common/libidset/idset.h"
+#include "src/common/libccan/ccan/str/str.h"
 
 #include "job_state.h"
 #include "idsync.h"
@@ -390,6 +391,34 @@ static const char *parse_job_name (const char *path)
     return path;
 }
 
+static int parse_per_resource (struct list_ctx *ctx,
+                               struct job *job,
+                               json_t *jobspec,
+                               const char **type,
+                               int *count)
+{
+    json_error_t error;
+
+    if (json_unpack_ex (jobspec, &error, 0,
+                        "{s:{s:{s?:{s?:{s?:{s?:s s?:i}}}}}}",
+                        "attributes",
+                        "system",
+                        "shell",
+                        "options",
+                        "per-resource",
+                        "type",
+                        type,
+                        "count",
+                        count) < 0) {
+        flux_log (ctx->h, LOG_ERR,
+                  "%s: job %ju invalid jobspec: %s",
+                  __FUNCTION__, (uintmax_t)job->id, error.text);
+        return -1;
+    }
+
+    return 0;
+}
+
 static int jobspec_parse (struct list_ctx *ctx,
                           struct job *job,
                           const char *s)
@@ -501,20 +530,39 @@ static int jobspec_parse (struct list_ctx *ctx,
     if (res[1].with && parse_res_level (ctx, job, res[1].with, &res[2]) < 0)
         goto nonfatal_error;
 
-    /* Set job->nnodes if available.  In jobspec version 1, only if
-     * resources listed as node->slot->core->NIL
-     */
     if (res[0].type != NULL && !strcmp (res[0].type, "node")
         && res[1].type != NULL && !strcmp (res[1].type, "slot")
         && res[2].type != NULL && !strcmp (res[2].type, "core")
-        && res[2].with == NULL)
+        && res[2].with == NULL) {
+        const char *type = NULL;
+        int count = 0;
+
+        /* Set job->nnodes if available.  In jobspec version 1, only if
+         * resources listed as node->slot->core->NIL
+         */
         job->nnodes = res[0].count;
 
-    /* Set job->ntasks
+        /* per-resource is used to overcome short-term gaps in
+         * Jobspec V1.  Remove per-resource logic below when it
+         * has been retired
+         */
+
+        if (parse_per_resource (ctx, job, jobspec, &type, &count) < 0)
+            goto nonfatal_error;
+
+        /* if per-resource.type == "node" and per-resource.count > 0
+         * there is an adjustment on ntasks.
+         */
+        if (type && streq (type, "node") && count > 0)
+            job->ntasks = res[0].count * count;
+    }
+
+    /* Set job->ntasks if not yet set
      */
-    if (json_unpack_ex (tasks, NULL, 0,
-                        "[{s:{s:i}}]",
-                        "count", "total", &job->ntasks) < 0) {
+    if (job->ntasks < 0
+        && json_unpack_ex (tasks, NULL, 0,
+                           "[{s:{s:i}}]",
+                           "count", "total", &job->ntasks) < 0) {
         int per_slot, slot_count = 0;
 
         if (json_unpack_ex (tasks, &error, 0,
