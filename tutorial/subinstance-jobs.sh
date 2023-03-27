e934100cacab16a08b780e41e9cab983d40827a6
Albert Chu <chu11@llnl.gov>
2023-03-27 06:47:51 -0700

    fdsafd

--- tutorial/subinstance-jobs.sh
+++ tutorial/subinstance-jobs.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-jobid1=`flux mini submit -n1 sleep inf`
-jobid2=`flux mini submit -n1 sleep inf`
-jobid3=`flux mini submit -n1 sleep inf`
-jobid4=`flux mini submit -n1 sleep inf`
-flux job status ${jobid1} ${jobid2} ${jobid3} ${jobid4}
+jobid1=`flux mini submit -n1 --job-name=Level2 sleep inf`
+jobid2=`flux mini submit -n1 --job-name=Level2 sleep inf`
+flux job status ${jobid1} ${jobid2}
