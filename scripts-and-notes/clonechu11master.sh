cfa3f0114b8af25bf52acb2e45a6926a7c6c9853
Albert Chu <chu11@llnl.gov>
2023-04-14 21:51:49 -0700

    fdsafd

--- scripts-and-notes/clonechu11master.sh
+++ scripts-and-notes/clonechu11master.sh
@@ -1,24 +1,8 @@
 #!/bin/sh
 
-DIRS="\
-  flux-core \
-  flux-sched \
-  flux-security \
-  rfc \
-  flux-workflow-examples \
-  flux-framework.github.io \
-  flux-hierarchy \
-  flux-scalability \
-  flux-docs \
-  flux-accounting \
-  flux-k8s \
-  flux-k8s-orchestrator \
-  flux-pmix \
-  flux-pam \
-  planning \
-"
+source ./fluxdirs.sh
 
-for dir in ${DIRS}
+for dir in ${FLUXDIRS}
 do
     echo "***************"
     echo "* Clone out $dir"
---
+++ scripts-and-notes/fluxdirs.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+FLUXDIRS="\
+  flux-core \
+  flux-sched \
+  flux-security \
+  rfc \
+  flux-workflow-examples \
+  flux-framework.github.io \
+  flux-hierarchy \
+  flux-scalability \
+  flux-docs \
+  flux-accounting \
+  flux-k8s \
+  flux-k8s-orchestrator \
+  flux-pmix \
+  flux-pam \
+  flux-coral2 \
+  flux-operator \
+  flux-proposals \
+  planning \
+  cheat-sheet \
+  dyad \
+  PerfFlowAspect \
+  fractale \
+  flux-restful-api \
+  FluxRM.jl \
+  actions \
+  flux-depend \
+  flux-power-mgr \
+"
