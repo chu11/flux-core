e918f552273cc8f75ecb1dddbf0d7ea36cef3359
Albert Chu <chu11@llnl.gov>
2023-09-22 14:45:10 -0700

    update rebuild

--- scripts-and-notes/rebuild.sh
+++ scripts-and-notes/rebuild.sh
@@ -14,5 +14,8 @@ if test -z "${proc}"; then
     proc=`cat /proc/cpuinfo | grep processor | wc -l`
     proc=$((${proc} / 2))
 fi
-./autogen.sh; ./configure; make clean; make distclean; ./autogen.sh; ./configure; make -j${proc}; make -j${proc} check
+./autogen.sh; ./configure; make clean; make distclean; ./autogen.sh; ./configure; make -j${proc}
+rm -f machine.xml
+hwloc-ls --of xml >machine.xml
+FLUX_HWLOC_XMLFILE=$(pwd)/machine.xml make -j${proc} check
 git log  --pretty=oneline | head -n 1
